80f237e4890918d159a13f616a79b6a658f1017a
Thiago Costa <thiscosta@live.com>
2019-04-15 16:32:39 -0300

    Mobile app schedule list

--- scheduler-app/App.js
+++ scheduler-app/App.js
@@ -1,50 +1,91 @@
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, ToastAndroid, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, ToastAndroid, Button, FlatList, ScrollView } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements'
 import SocketIOClient from 'socket.io-client'
 import OneSignal from 'react-native-onesignal'
+import axios from 'axios'
+import moment from 'moment'
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
 
-    this.socket = SocketIOClient('http://192.168.0.99:3030/')
+    // this.socket = SocketIOClient('http://192.168.0.99:3030/')
 
-    this.entrar = this.entrar.bind(this)
+    // this.entrar = this.entrar.bind(this)
+    // this.socket.on('newSchedule', (message) => {
+    //   console.log('chegou a mensagem')
+    //   ToastAndroid.show(message, ToastAndroid.SHORT);
+    // })
 
-    this.socket.on('newSchedule', (message) => {
-      console.log('chegou a mensagem')
-      ToastAndroid.show(message, ToastAndroid.SHORT);
-    })
+    this.searchSchedules = this.searchSchedules.bind(this)
+
+    this.state = {
+      scheduleList: []
+    }
   }
 
-  componentDidMount(){
+  async componentDidMount() {
     OneSignal.init('84af63b9-4b44-4094-9931-dbfcec94781a')
     OneSignal.addEventListener('received', this.receivedPush)
+
+    await this.searchSchedules()
   }
 
-  receivedPush(push){
+  async searchSchedules() {
+    const response = await axios.get('http://192.168.0.99:3030/establishments/5cb4677456a51101743373e5/schedules');
+    console.log('schedules ', response)
+    await this.setState({ scheduleList: (response.data.reverse()) })
+  }
+
+  receivedPush = async (push) => {
     console.log('Recebeu a notificação: ' + push)
+    await this.searchSchedules()
   }
 
-  entrar(){
+  entrar() {
     let establishment = { id: '5cb4677456a51101743373e5' }
     console.log('vai entrar')
-    try{
+    try {
       this.socket.emit("join", establishment)
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Button onPress={this.entrar} title="Entrar" />
-      </View>
+        <View style={{
+          flex: 1, flexDirection: 'column'
+        }}>
+          <FlatList
+            data={this.state.scheduleList}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) =>
+              <ListItem
+                key={item.id}
+                title={`Agenda: Usuário`}
+                subtitle={
+                  <View>
+                    <Text>{moment(item.date).format('DD/MM/YYYY - HH:mm')}</Text>
+                  </View>
+                }
+                leftIcon={<Icon
+                  reverse
+                  name='ios-american-football'
+                  type='ionicon'
+                  color='#517fa4'
+                />} />
+            }
+          />
+        </View>
+      </ScrollView>
     )
   }
 
@@ -53,8 +94,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
